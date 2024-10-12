1e0537cd81f9ce34138870306538b47c74da8bed
NearlyDragonss <nyxrkd@gmail.com>
2024-10-12 14:39:04 +0100

    Arrows move down and dissapear when arrow is clicked

---
+++ scenes/sprite_arrow.gd
@@ -0,0 +1,21 @@
+extends Sprite2D
+
+var speed = 100
+
+var velocity = Vector2.ZERO
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	velocity = Vector2.DOWN * speed
+	
+	position += velocity * delta
+	
+	if Input.is_action_pressed("ui_left"):
+		visible = not visible
+		
+		
