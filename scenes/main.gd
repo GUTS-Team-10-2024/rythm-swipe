a722ef218cb4ec9f0329021e3e2c9b2229c8a9db
andrei <andrei.ghita2002@gmail.com>
2024-10-12 17:46:44 +0100

    added direction to arrows

--- scenes/main.gd
+++ scenes/main.gd
@@ -30,7 +30,6 @@ func _on_spawn_timer_timeout() -> void:
 	var d = randi_range(0, 5)
 	if d == 0:
 		arrow_spawn_location.progress_ratio = left_arrow_spawn_position
-		# TODO set the tyoe (direction) of the arrow
 	elif d == 1:
 		arrow_spawn_location.progress_ratio = up_arrow_spawn_position
 	elif d == 2:
@@ -40,6 +39,7 @@ func _on_spawn_timer_timeout() -> void:
 	else:
 		arrow_spawn_location.progress_ratio = right_arrow_spawn_position
 	new_arrow.position = arrow_spawn_location.position
+	new_arrow.direction = d
 	
 	add_child(new_arrow) # now the arrow becomes active
 
