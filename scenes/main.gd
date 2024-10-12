ed641c22749371c19f688ebfb44975e1a55374e2
andrei <andrei.ghita2002@gmail.com>
2024-10-12 15:25:56 +0100

    Arrow Generator

---
+++ scenes/main.gd
@@ -0,0 +1,40 @@
+extends Node2D
+
+@export var arrow_scene: PackedScene
+var score
+@export var left_arrow_spawn_position  = 0.20
+@export var up_arrow_spawn_position    = 0.40
+@export var down_arrow_spawn_position  = 0.60
+@export var right_arrow_spawn_position = 0.80
+
+func _ready() -> void:
+	pass
+
+func _process(delta: float) -> void:
+	pass
+
+func new_game() -> void:
+	score = 0
+	$SpawnTimer.start()
+
+func game_over() -> void:
+	print(score)
+	$SpawnTimer.stop()
+
+# Spawn Timer Tick
+func _on_spawn_timer_timeout() -> void:
+	var new_arrow = arrow_scene.instantiate()
+	var arrow_spawn_location = $SpawnPath/SpawnLocation
+	var d = randi_range(0, 4)
+	if d == 0:
+		arrow_spawn_location.progress_ratio = left_arrow_spawn_position
+	elif d == 1:
+		arrow_spawn_location.progress_ratio = up_arrow_spawn_position
+	elif d == 2:
+		arrow_spawn_location.progress_ratio = down_arrow_spawn_position
+	else:
+		arrow_spawn_location.progress_ratio = right_arrow_spawn_position
+	new_arrow.position = arrow_spawn_location
+	
+	add_child(new_arrow) # now the arrow becomes active
+	
---
+++ scenes/spawn_timer.gd
@@ -0,0 +1,8 @@
+extends Timer
+
+
+func _ready() -> void:
+	pass
+
+func _process(delta: float) -> void:
+	pass
