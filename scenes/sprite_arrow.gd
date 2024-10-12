2797813eae7b853fe9728bfc47fb4a9236bae4ec
NearlyDragonss <nyxrkd@gmail.com>
2024-10-12 15:56:11 +0100

    stuff

--- scenes/sprite_arrow.gd
+++ scenes/sprite_arrow.gd
@@ -2,6 +2,7 @@ extends Area2D
 signal hit
 
 @export var speed = 100
+@export var direction = 0
 var screen_size
 
 var velocity = Vector2.ZERO
@@ -23,7 +24,7 @@ func _process(delta: float) -> void:
 		
 
 
-func _on_area_shape_entered(area_rid: RID, area: Area2D, area_shape_index: int, local_shape_index: int) -> void:
-	hide()
-	hit.emit()
-	$CollisionShape2D.set_deferred("disabled", true)
+#func _on_area_shape_entered(area_rid: RID, area: Area2D, area_shape_index: int, local_shape_index: int) -> void:
+	#hide()
+	#hit.emit()
+	#$CollisionShape2D.set_deferred("disabled", true)
