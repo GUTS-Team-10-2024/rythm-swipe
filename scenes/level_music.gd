ec8fb422d714a490a834641d51e18e432b018553
2674244m <2674244M@student.gla.ac.uk>
2024-10-13 09:45:19 +0100

    added new music and speed change

--- scenes/level_music.gd
+++ scenes/level_music.gd
@@ -1,21 +1,24 @@
 extends AudioStreamPlayer2D
 
 # Variables to control the BPM increase
-var target_bpm = 180
 var initial_bpm = 120
-var duration = 180.0 # Duration over which the BPM will increase (in seconds)
+var rate_of_increase = 0.5  # Rate at which the BPM increases per second
 var current_time = 0.0
 
-var song_list = ["thick-of-it.mp3","what-is-love.mp3"]
+var song_list = ["copyright-free.mp3","hey-you.mp3","again-sometime.mp3","leap-of-faith.mp3","otis-mcmusic.mp3"]
 
 var song_bpm = {
-	"thick-of-it.mp3": 146,
-	"what is love.mp3": 124
+	"copyright-free.mp3": 124,
+	"hey-you.mp3":120,
+	"again-sometime.mp3":120,
+	"leap-of-faith.mp3":140,
+	"otis-mcmusic.mp3": 184
 }
 
 func _ready():
 	
 	var random_song = get_random_song(song_list)
+	initial_bpm = song_bpm[random_song]
 	print(random_song)
 	load_song(random_song)	
 	play()
@@ -26,11 +29,10 @@ func get_random_song(songs):
 	return songs[random_index]
 
 func _process(delta):
-	if current_time < duration:
-		current_time += delta
-		# Calculate the new pitch scale based on the elapsed time
-		var new_bpm = initial_bpm + (target_bpm - initial_bpm) * (current_time / duration)
-		pitch_scale = new_bpm / initial_bpm
+	# Continuously increase the pitch scale
+	var new_bpm = initial_bpm + rate_of_increase * current_time
+	pitch_scale = new_bpm / initial_bpm
+	current_time += delta
 
 func load_song(song_name):
 	stream = load("res://songs/" + song_name)
